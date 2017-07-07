7a7d0a63fcf87cb8c7cfeada8e05e56f864196f3
Kyle Lamse <kyle.lamse@cgi.com>
2017-07-07 11:28:11 -0600

    Added css loading

--- src/components/Hello/Hello.tsx
+++ src/components/Hello/Hello.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
+import './hello.css';
+
 
 interface HelloProps { compiler: string; framework: string; }
-const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+const Hello = (props: HelloProps) => <h1 className="header">Hello from {props.compiler} and {props.framework}!</h1>;
 
 export {
     Hello,
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,14 @@
 import { Configuration } from 'webpack';
+import * as webpack from 'webpack';
+import * as ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const config: Configuration = {
-    entry: "./src/index.tsx",
+    entry: { 
+        app: './src/index.tsx',
+        vendor : ['react', 'react-dom']
+    },
     output: {
-        filename: "bundle.js",
+        filename: "[name].js",
         path: __dirname + "/dist"
     },
 
@@ -17,22 +22,29 @@ const config: Configuration = {
 
     module: {
         rules: [
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            // Extract css files into one giant file
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader'
+                })
+            }
         ]
     },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: Infinity
+        }),
+        new ExtractTextPlugin("styles.css")
+    ]
 };
 
 export default config;
