e251e6375c2857f20a7f057264092c6a1905a84e
Kyle Lamse <kyle.lamse@cgi.com>
2017-07-08 09:51:25 -0600

    Indentation rule

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,15 @@ import * as ReactDOM from 'react-dom';
 
 import { Hello } from './components/Hello';
 
+if (true) {
+    console.log('hello');
+}
+
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
     document.getElementById('root')
 );
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
