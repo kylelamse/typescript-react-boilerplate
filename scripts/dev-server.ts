414739a0e91b7fc8adcc1b64380bc4cb0b44a281
Kyle Lamse <kyle.lamse@cgi.com>
2017-08-04 21:36:04 -0500

    Cleanup dev-server script

--- scripts/dev-server.ts
+++ scripts/dev-server.ts
@@ -1,22 +1,26 @@
 import * as express from 'express';
+import { Application } from 'express';
 import * as webpackDevMiddleware from 'webpack-dev-middleware';
 import * as webpackHotMiddleware from 'webpack-hot-middleware';
 import * as webpack from 'webpack';
+import { Compiler } from 'webpack';
 
 import webpackConfig from '../config/webpack.dev.config';
 import webpackDevServerConfig from '../config/webpack.server.config';
 import paths from '../config/paths';
 
-const app = express();
+const port: number = 8080;
+
+const app: Application = express();
 
 // have express serve static files
 app.use(paths.staticPath, express.static(paths.public));
 
-const compiler = webpack(webpackConfig);
+const compiler: Compiler = webpack(webpackConfig);
 
 // allow webpack to inject output to server
 app.use(webpackDevMiddleware(compiler, webpackDevServerConfig));
 
 app.use(webpackHotMiddleware(compiler));
 
-app.listen(8080);
+app.listen(port);
